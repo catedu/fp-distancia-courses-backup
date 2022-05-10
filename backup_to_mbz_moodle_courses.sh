bf6687cd167f1240ce15b4d3ac82957d6fae8722
Pablo Ruiz Soria <pabloruizsoria@gmail.com>
2022-05-10 13:02:57 +0200

    Añado nuevos comentarios y hago que ruta destino dependa de fecha actual

--- backup_to_mbz_moodle_courses.sh
+++ backup_to_mbz_moodle_courses.sh
@@ -6,12 +6,23 @@
 # Código en github: https://github.com/catedu/fp-distancia-courses-backup
 #
 
+#
+LOCALROOT=$(pwd)
+DATE=$( date -u +"%Y%m%d" )
+DESTINO="${LOCALROOT}/zz_mbzs_${DATE}"
 
-# Se obtiene el listado de cursos
-CURSOS=`moosh -n -p /var/www/html course-list`
+echo "${DESTINO}"
+echo "$DESTINO"
+
+# Creo la ruta donde dejaté los ficheros
+[ ! -d ${DESTINO} ] && sudo mkdir -p ${DESTINO}
+
+# Se obtiene el listado de cursos quitando la 1era línea
+CURSOS=`moosh -n -p /var/www/html course-list | tail -n +2`
 
 # Para cada curso previamente obtenido
 while IFS= read -r linea; do
+  echo "Procesando la línea:"
   echo "-> $linea <-";
   # Obtengo el ID procesando la línea
 
@@ -20,18 +31,20 @@ while IFS= read -r linea; do
   SHORTNAME=`echo $linea | cut -d , -f 3`
 
   # Al ID le quito aquello que sobra
-  echo "ID: $ID"
+  # echo "ID: $ID"
   ID=`echo "$ID" | sed 's/^.//;s/.$//'`
-  echo "ID: $ID"
+  # echo "ID: $ID"
 
   # Al shortname le quito aquello que sobra
-  echo "Shortname: $SHORTNAME"
+  #echo "Shortname: $SHORTNAME"
   SHORTNAME=`echo "$SHORTNAME" | sed 's/^.//;s/.$//'`
 
   # Creo el nombre del fichero
   NOMBRE=`echo ${SHORTNAME}.mbz`
-  echo "NOMBRE: $NOMBRE"
-
+  #echo "NOMBRE: $NOMBRE"
+  echo "Exportando el curso ${SHORTNAME} (id $ID)... (se paciente, hay cursos de gran tamaño)"
   # Realizo la exportanción del curso de id $ID en la ruta indicada con el nombre $NOMBRE creado
-  moosh -n -p /var/www/html course-backup -F -f /var/www/html/zz_mbzs_20210920/$NOMBRE $ID
+  moosh -n -p /var/www/html course-backup -F -f ${DESTINO}/$NOMBRE $ID
 done <<< "$CURSOS"
+
+echo "Proceso terminado"
